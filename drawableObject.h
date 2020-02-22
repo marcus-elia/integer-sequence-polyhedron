1d29fed2ad620d997852dc4da315bb39cb193a68
marcus-elia <mselia@uvm.edu>
2020-02-22 07:56:47 -0500

    Added the DrawableObject class, which has a center point and a color. The header now contains the definition of both of those structs.

---
+++ drawableObject.h
@@ -0,0 +1,33 @@
+#ifndef INC_3D_GRAPHICS_DRAWABLEOBJECT_H
+#define INC_3D_GRAPHICS_DRAWABLEOBJECT_H
+
+struct RGBAcolor
+{
+    double r;
+    double g;
+    double b;
+    double a;
+};
+
+
+struct point
+{
+    double x;
+    double y;
+    double z;
+};
+
+class DrawableObject
+{
+private:
+    RGBAcolor color;
+    point center;
+public:
+    DrawableObject();
+    DrawableObject(point inputCenter, RGBAcolor inputColor);
+    virtual void draw() const = 0;
+    virtual void move(double delta_x, double delta_y, double delta_z) = 0;
+};
+
+
+#endif //INC_3D_GRAPHICS_DRAWABLEOBJECT_H
