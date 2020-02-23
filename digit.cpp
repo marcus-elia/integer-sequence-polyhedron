65fb7d0f348cc428443d1af4c71bbea4ce3c42b8
marcus-elia <mselia@uvm.edu>
2020-02-23 11:55:25 -0500

    Defined the segments vector for each digits (these are static)

--- digit.cpp
+++ digit.cpp
@@ -1,5 +1,15 @@
 #include "digit.h"
 
+std::vector<int>Digit::segments0{1,2,3,4,5,6};
+std::vector<int>Digit::segments1{2,3};
+std::vector<int>Digit::segments2{1,2,7,5,4};
+std::vector<int>Digit::segments3{1,2,7,3,4};
+std::vector<int>Digit::segments4{6,7,2,3};
+std::vector<int>Digit::segments5{1,6,7,3,4};
+std::vector<int>Digit::segments6{1,3,4,5,6,7};
+std::vector<int>Digit::segments7{1,2,3};
+std::vector<int>Digit::segments8{1,2,3,4,5,6,7};
+std::vector<int>Digit::segments9{1,2,3,4,6,7};
 
 Digit::Digit() : DrawableObject()
 {
