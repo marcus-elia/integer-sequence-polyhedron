232d3d361f79f7467fb3aa3300277d9f0ddc75a4
marcus-elia <mselia@uvm.edu>
2020-02-25 13:54:15 -0500

    Corrected the segments for the number 6 and deleted old code

--- digit.cpp
+++ digit.cpp
@@ -1,18 +1,8 @@
 #include "digit.h"
 
-/*std::vector<int>Digit::segments0{1,2,3,4,5,6};
-std::vector<int>Digit::segments1{2,3};
-std::vector<int>Digit::segments2{1,2,7,5,4};
-std::vector<int>Digit::segments3{1,2,7,3,4};
-std::vector<int>Digit::segments4{6,7,2,3};
-std::vector<int>Digit::segments5{1,6,7,3,4};
-std::vector<int>Digit::segments6{1,3,4,5,6,7};
-std::vector<int>Digit::segments7{1,2,3};
-std::vector<int>Digit::segments8{1,2,3,4,5,6,7};
-std::vector<int>Digit::segments9{1,2,3,4,6,7};*/
 std::vector<std::vector<int>>Digit::segmentsVector{{1,2,3,4,5,6},{2,3},{1,2,7,5,4},
                                                    {1,2,7,3,4},{6,7,2,3},{1,6,7,3,4},
-                                                   {1,2,3,5,6,7},{1,2,3},{1,2,3,4,5,6,7},
+                                                   {1,4,3,5,6,7},{1,2,3},{1,2,3,4,5,6,7},
                                                    {1,2,3,4,6,7}};
 
 Digit::Digit() : DrawableObject()
@@ -21,7 +11,6 @@ Digit::Digit() : DrawableObject()
     width = 10;
     height = 20;
     thickness = 4;
-    //initializeSegments();
 }
 
 Digit::Digit(point inputCenter, RGBAcolor inputColor, int inputDigit, int inputWidth, int inputHeight) :
@@ -31,7 +20,6 @@ Digit::Digit(point inputCenter, RGBAcolor inputColor, int inputDigit, int inputW
     width = inputWidth;
     height = inputHeight;
     thickness = 4;
-    //initializeSegments();
 }
 
 Digit::Digit(const Digit &oldDigit)
@@ -40,7 +28,6 @@ Digit::Digit(const Digit &oldDigit)
     width = oldDigit.width;
     height = oldDigit.height;
     thickness = oldDigit.thickness;
-    //initializeSegments();
 }
 
 Digit::~Digit()
@@ -49,32 +36,6 @@ Digit::~Digit()
     //segments = nullptr;
 }
 
-/*void Digit::initializeSegments()
-{
-    switch(digit)
-    {
-        case 0 : segments = &segments0;
-            break;
-        case 1 : segments = &segments1;
-            break;
-        case 2 : segments = &segments2;
-            break;
-        case 3 : segments = &segments3;
-            break;
-        case 4 : segments = &segments4;
-            break;
-        case 5 : segments = &segments5;
-            break;
-        case 6 : segments = &segments6;
-            break;
-        case 7 : segments = &segments7;
-            break;
-        case 8 : segments = &segments8;
-            break;
-        case 9 : segments = &segments9;
-            break;
-    }
-}*/
 
 void Digit::draw() const
 {
