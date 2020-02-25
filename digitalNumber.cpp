bbc664c8d002a5471609bdea74048810715ea6ab
marcus-elia <mselia@uvm.edu>
2020-02-25 15:04:12 -0500

    The DigitalNumber class now contains a vector of Digits, and puts them where they belong.

--- digitalNumber.cpp
+++ digitalNumber.cpp
@@ -4,17 +4,60 @@ DigitalNumber::DigitalNumber() : DrawableObject()
 {
     number = 0;
 }
-DigitalNumber::DigitalNumber(point inputCenter, RGBAcolor inputColor, int inputNumber) :
+DigitalNumber::DigitalNumber(point inputCenter, RGBAcolor inputColor, int inputNumber, int inputMaxWidth, int inputMaxHeight) :
                DrawableObject(inputCenter, inputColor)
 {
     number = inputNumber;
+    maxWidth = inputMaxWidth;
+    maxHeight = inputMaxHeight;
 }
 
-void DigitalNumber::drawDigit(point position, int number)
+void DigitalNumber::initializeDigits()
 {
+    // First, get a list of digits. So 341 becomes {1,4,3}
+    std::vector<int> decimalDigits;
+    decimalDigits.push_back(number % 10);
+    int x = number - number % 10;
+    x = x / 10;
+    while(x != 0)
+    {
+        decimalDigits.push_back(x % 10);
+        x = x - x % 10;
+        x = x / 10;
+    }
 
+    // Now create Digit objects in the correct locations
+    int d = decimalDigits.size();
+    double digitWidth = 0.8 * maxWidth / (1.2*d - 0.2); // How wide each one is
+    double digitHeight = 2*digitWidth;                  // How tall they are
+    if(digitHeight > maxHeight)
+    {
+        digitHeight = maxHeight;
+        digitWidth = digitHeight/2;
+    }
+    double gapSize = 0.2 * digitWidth;                  // How far apart they are
+
+    double curCenterX;   // Keep track of we are putting Digits
+    if(d % 2 == 0)
+    {
+        curCenterX = center.x + gapSize/2.0 + (d/2.0-1)*digitWidth + (d/2.0-1)*gapSize + digitWidth/2.0;
+    }
+    else
+    {
+        curCenterX = center.x + (d-1)/2*gapSize + (d-1)/2*digitWidth;
+    }
+    for(int i : decimalDigits)
+    {
+        digits.push_back(Digit({curCenterX, center.y, center.z}, color, i, digitWidth, digitHeight));
+        curCenterX -= gapSize;
+        curCenterX -= digitWidth;
+    }
 }
+
 void DigitalNumber::draw()
 {
-    
+    for(Digit d : digits)
+    {
+        d.draw();
+    }
 }
--- digitalNumber.h
+++ digitalNumber.h
@@ -3,16 +3,26 @@
 
 #include "drawableObject.h"
 #include "graphics.h"
+#include "digit.h"
 
 class DigitalNumber : public DrawableObject
 {
 private:
+    // The number we are representing
     int number;
+
+    // The dimensions we have to draw the number in
+    int maxWidth;
+    int maxHeight;
+
+    std::vector<Digit> digits;
 public:
     DigitalNumber();
-    DigitalNumber(point inputCenter, RGBAcolor inputColor, int inputNumber);
+    DigitalNumber(point inputCenter, RGBAcolor inputColor, int inputNumber, int inputMaxWidth, int inputMaxHeight);
 
-    void drawDigit(point position, int number);
+    // Create the vector of Digit objects
+    void initializeDigits();
+
     void draw();
 };
 
